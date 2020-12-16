d2759446c82a10813945c4ff51d3b0d80dd47102
Matthew <Matthewtroberts10@gmail.com>
2020-12-15 23:45:24 -0600

    Tuesday/Monday classwork

---
+++ week06/2-Tuesday/lectureNotes/affterLnhc.js
@@ -0,0 +1,26 @@
+// let fib = (n) => {
+//     if( n < 2){
+//         return n;
+//     }
+//     return fib(n-1) + fib(n-2); 
+// }
+// console.log(fib(10));
+
+// let sum = 10;
+
+// for( i = 0; i < sum; i++){
+
+// sum += 1;
+
+// }
+// console.log(sum);
+
+// console.log("before set timeout");
+
+// setTimeout(() =>{
+//     console.log("inside of the set timeout")
+// },1000)
+
+//     console.log("outside");
+
+ //api.openweathermap.org/data/2.5/weather?q=Houston}&appid=267a190819c28864a13fbcef0569c136
